95cfe1c776f2a634a75d9952ed8b328110e49039
PateyV <valentin.patey@etu.unicaen.fr>
2024-03-04 16:36:20 +0100

    commit

--- controle-qualite.bat
+++ controle-qualite.bat
@@ -1,4 +1,4 @@
-SET MAVEN_HOME=c:\Data\maven
+SET MAVEN_HOME=C:\Data\Maven\apache-maven-3.9.6
 SET JAVA_HOME=C:\Program Files\Java\jdk-11.0.11
 SET PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%PATH%
 REM Lancer à la racine du projet
